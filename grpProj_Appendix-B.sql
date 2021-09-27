257338d5ed72e9033c07db4d2187fe3655ec0600
Joshua <61338348+suenalaba@users.noreply.github.com>
2021-09-27 13:13:53 +0800

    Update grpProj_Appendix-B.sql

--- grpProj_Appendix-B.sql
+++ grpProj_Appendix-B.sql
@@ -11,7 +11,9 @@ SELECT SUM(population) AS total_asean_population_top_ten FROM (SELECT MAX(CAST(I
 #########################################################################
 ####### 3.	Generate a list of unique data sources (source_name). #######
 #########################################################################
-SELECT DISTINCT source_name FROM country_vaccinations ORDER BY source_name;
+SELECT DISTINCT source_name 
+FROM country_vaccinations 
+ORDER BY source_name;
 
 ##################################################################################################################################################
 ####### 4.	Specific to Singapore, display the daily total_vaccinations starting (inclusive) March-1 2021 through (inclusive) May-31 2021. #######
