ef5b1d098b8e17fa5e5ae3280cfc6190229e9afd
zacjay99 <zhengjie.ng@gmail.com>
2021-10-26 16:48:16 +0800

    Added nosql Q6-Q10(except Q9)

--- country_vaccinations_query_copy.js
+++ country_vaccinations_query_copy.js
@@ -131,3 +131,49 @@ db.country_vaccinations_by_manufacturer.aggregate(
 
 /* 6.	What is the country with the most types of administrated vaccine?
 [source table: country_vaccinations_by_manufacturer] */
+db.country_vaccinations_by_manufacturer.aggregate([
+    {$group:{_id:{location:"$location",vaccine:"$vaccine"}}},
+    {$group:{_id:{location:"$_id.location"},all_vaccine:{$push:"$_id.vaccine"},count:{$sum:1}}},
+    {$sort:{count:-1}},
+    {$limit:1},
+    {$unwind: "$all_vaccine"},
+    {$project:{_id:0,"location":"$_id.location","vaccine":"$all_vaccine"}}
+    ])
+    
+/* 7.   What are the countries that have fully vaccinated more than 60% of its people? For each
+country, display the vaccines administrated.
+[source table: country_vaccinations] */
+db.country_vaccinations.aggregate([
+    {$group:{_id:{country:"$country"}, vaccines:{$max:"$vaccines"}, vaccination_percentage:{$max:"$people_fully_vaccinated_per_hundred_cleaned"}}},
+    {$match:{"vaccination_percentage":{$gt:60}}},
+    {$project:{_id:0, "country":"$_id.country", vaccines:1, vaccination_percentage:1}},
+    
+])
+
+/* 8. Monthly vaccination insight – display the monthly total vaccination amount of each
+vaccine per month in the United States.
+[source table: country_vaccinations_by_manufacturer]*/
+db.country_vaccinations_by_manufacturer.aggregate([
+    {$match:{location:"United States"}},
+    {$project:{_id:0, month:{$month:"$date_cleaned"}, vaccine:1, total_vaccinations_cleaned:1}},
+    {$group:{_id:{month:"$month",vaccine:"$vaccine"}, monthly_total_vaccination:{$max:"$total_vaccinations_cleaned"}}}
+    ])
+// IDK how to change month to month name
+
+/* 9. Days to 50 percent. Compute the number of days (i.e., using the first available date on
+records of a country) that each country takes to go above the 50% threshold of
+vaccination administration (i.e., total_vaccinations_per_hundred > 50)
+[source table: country_vaccinations] */
+
+
+
+/* 10. Compute the global total of vaccinations per vaccine.
+[source table: country_vaccinations_by_manufacturer]*/
+db.country_vaccinations_by_manufacturer.aggregate([
+    {$group:{_id:{location:"$location",vaccine:"$vaccine"}, total_vaccination:{$max:"$total_vaccinations_cleaned"}}},
+    {$group:{_id:{vaccine:"$_id.vaccine"}, global_total:{$sum:"$total_vaccination"}}},
+    {$project:{_id:0, vaccine:"$_id.vaccine", global_total:"$global_total"}},
+    {$sort:{global_total:-1}}
+    ])
+
+
