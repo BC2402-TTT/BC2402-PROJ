d0a75256b966fd86e18f77c2c442f9c8ad168d08
TanJunHong <b190013@e.ntu.edu.sg>
2021-11-03 18:56:05 +0800

    Revert

--- noSQL/NOSQL Queries.js
+++ noSQL/NOSQL Queries.js
@@ -5,8 +5,8 @@ use country_vaccinations
 [source table: country_vaccinations] */
 db.country_vaccinations.find(
     {country: "Singapore"},
-    {date:1, _id:0,total_vaccinations_cleaned:1}
-)
+    {date_cleaned:1, _id:0,total_vaccinations_cleaned:1}
+).sort({date_cleaned: 1})
 
 
 /* 2.	Display the sum of daily vaccinations among ASEAN countries.
@@ -22,15 +22,15 @@ db.country_vaccinations.aggregate(
 /* 3.	Identify the maximum daily vaccinations per million of each country. Sort the list based on daily vaccinations per million in a descending order.
 [source table: country_vaccinations] */
 db.country_vaccinations.aggregate(
-    {$group: {_id:"$country", max_daily_vaccinations_per_million: {$max: "$daily_vaccinations_per_million_cleaned"} } }
-    {$sort: {"max_daily_vaccinations_per_million_cleaned":-1}},
+    {$group: {_id:"$country", max_daily_vaccinations_per_million: {$max: "$daily_vaccinations_per_million_cleaned"} } },
+    {$sort: {"max_daily_vaccinations_per_million":-1}},
     {$project: {"country": 1, "max_daily_vaccinations_per_million":1}}
 )
 
 /* 4.	Which is the most administrated vaccine? Display a list of total administration (i.e., sum of total vaccinations) per vaccine.
 [source table: country_vaccinations_by_manufacturer] */
 db.country_vaccinations_by_manufacturer.aggregate(
-    {$group: {_id:"$vaccine", total_administrated: {$sum: "$total_vaccinations_cleaned"} } }
+    {$group: {_id:"$vaccine", total_administrated: {$sum: "$total_vaccinations_cleaned"} } },
     {$sort: {"total_administrated":-1}},
     {$project: {"vaccine": 1, "total_administrated":1}}
 )
@@ -40,7 +40,7 @@ db.country_vaccinations_by_manufacturer.aggregate(
 db.country_vaccinations_by_manufacturer.aggregate(
     {$match:{location: "Italy" } }, 
     {$group:{_id:"$vaccine", date:{$min:"$date_cleaned"} } },
-    {$sort: {"date_cleaned" : 1 } }
+    {$sort: {"date_cleaned" : 1 } },
     {$project: {"vaccine" : 1, "date" : 1} }
 )
 
@@ -53,7 +53,7 @@ db.country_vaccinations_by_manufacturer.aggregate([
     {$limit:1},
     {$unwind: "$all_vaccine"},
     {$project:{_id:0,"location":"$_id.location","vaccine":"$all_vaccine"}}
-    ])
+])
     
 /* 7.   What are the countries that have fully vaccinated more than 60% of its people? For each
 country, display the vaccines administrated.
@@ -62,7 +62,7 @@ db.country_vaccinations.aggregate([
     {$group:{_id:{country:"$country"}, vaccines:{$max:"$vaccines"}, vaccination_percentage:{$max:"$people_fully_vaccinated_per_hundred_cleaned"}}},
     {$match:{"vaccination_percentage":{$gt:60}}},
     {$project:{_id:0, "country":"$_id.country", vaccines:1, vaccination_percentage:1}},
-    
+    {$sort:{vaccination_percentage:-1}}
 ])
 
 /* 8. Monthly vaccination insight – display the monthly total vaccination amount of each
@@ -72,7 +72,7 @@ db.country_vaccinations_by_manufacturer.aggregate([
     {$match:{location:"United States"}},
     {$project:{_id:0, month:{$month:"$date_cleaned"}, vaccine:1, total_vaccinations_cleaned:1}},
     {$group:{_id:{month:"$month",vaccine:"$vaccine"}, monthly_total_vaccination:{$max:"$total_vaccinations_cleaned"}}}
-    ])
+])
 // IDK how to change month to month name
 
 /* 9. Days to 50 percent. Compute the number of days (i.e., using the first available date on
